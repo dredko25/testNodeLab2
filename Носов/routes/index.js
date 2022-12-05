1b7886c844a7037a2c75385197155b8956980f42
dredko25 <dredko25@gmail.com>
2022-12-05 15:08:19 +0100

    get

--- Носов/routes/index.js
+++ Носов/routes/index.js
@@ -14,9 +14,9 @@ function GET(req, res, url, payload) {
     let parsedObject = QueryString.parse(url);
     console.log("Parsed Query:", parsedObject);
     res.json({name: `Parsed Query: ${parsedObject}`});
-    // const ip = res.socket.remoteAddress;
-    // const port = res.socket.remotePort;
-    // res.json({name: `Path ${url.pathname}.Your IP address is ${ip} and your source port is ${port}.`})
+    const ip = res.socket.remoteAddress;
+    const port = res.socket.remotePort;
+    res.json({name: `Path ${url.pathname}.Your IP address is ${ip} and your source port is ${port}.`})
 }
 
 function OPTIONS(req, res, url, payload) {
